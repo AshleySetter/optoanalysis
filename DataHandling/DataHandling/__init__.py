3e0539c4bb4ab55036fe754ca1bfc2b751debde6
Ash Setter <ajs3g11@soton.ac.uk>
2016-11-25 13:48:08 +0000

    initial commit

---
+++ DataHandling/DataHandling/__init__.py
@@ -0,0 +1,19 @@
+"""
+DataHandling
+============
+
+Package of functions for the Matter-Wave Interferometry
+ group for handling experimental data.
+
+
+"""
+
+# init file
+
+#import DataHandling.LeCroy
+#import DataHandling.DataHandling
+
+from .LeCroy import *
+from .DataHandling import *
+
+
