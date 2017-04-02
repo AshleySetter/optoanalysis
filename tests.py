97ef21c8b45c86a6b21734444bf1786c42316d92
AshleySetter <ajs3g11@soton.ac.uk>
2017-04-02 01:03:14 +0100

    added new baseline image as travis tests are failing - maybe something do to with me using a mac to generate the baseline? or seaborn effecting it?

--- tests.py
+++
@@ -1,8 +0,0 @@
-import matplotlib
-matplotlib.use('agg')
-import pytest
-
-#pytest.main(['-v', '--cov', 'DataHandling', '--mpl-generate-path=baseline'])
-pytest.main(['-v', '--cov', 'DataHandling', '--mpl'])
-
-# runs pytest in verbose mode, reports coverage of the DataHandling library and with pytest-mpl so that the figures created are compared to the baselines in tests/baseline. I am running it inside of python so that the matplotlib.use('agg') command which sets the matplotlib DISPLAY variable is set before running pytest - otherwise plots cannot be made on travis-CI
