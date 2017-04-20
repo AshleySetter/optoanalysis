1bf2f04b0e9c717190aa9827b8f7c14c8b9494b7
AshleySetter <ajs3g11@soton.ac.uk>
2017-04-20 10:21:53 +0100

    changed to version 3.0.0 as I have broken backward compatibility by changing the argument order in get_fit and fit_PSD

--- datahandling/setup.py
+++ datahandling/setup.py
@@ -3,8 +3,18 @@ from distutils.core import setup
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+# the following 8 lines set the __version__ variable
+import pkg_resources as _pkg_resources
+
+_resource_package = __name__  # Gets the module/package name.
+_rawVersionFile = _pkg_resources.resource_string(
+                _resource_package, "VERSION")
+_decodedVersionFile = _rawVersionFile.decode(encoding='UTF-8')
+# decocdes the bytes object into a string object
+Version = _decodedVersionFile.splitlines()[0]
+    
 setup(name='datahandling',
-      version='2.0.0',
+      version=Version,
       description='Python package with functions for data analysis',
       author='Ashley Setter',
       author_email='A.Setter@soton.ac.uk',
