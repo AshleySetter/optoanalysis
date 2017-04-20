2a648d00dc095df870aa96be92c8412eae7e52c4
Ash Setter <ajs3g11@soton.ac.uk>
2017-04-20 11:12:32 +0100

    changed code so that in both __init__ and setup.py version is loaded from the VERSION file

--- datahandling/datahandling/__init__.py
+++ datahandling/datahandling/__init__.py
@@ -10,15 +10,11 @@ Package of functions for the Matter-Wave Interferometry
 
 # init file
 
-# the following 8 lines set the __version__ variable
-import pkg_resources as _pkg_resources
+import os
 
-_resource_package = __name__  # Gets the module/package name.
-_rawVersionFile = _pkg_resources.resource_string(
-            _resource_package, "VERSION")
-_decodedVersionFile = _rawVersionFile.decode(encoding='UTF-8')
-# decocdes the bytes object into a string object
-__version__ = _decodedVersionFile.splitlines()[0]
+_mypackage_root_dir = os.path.dirname(__file__)
+_version_file = open(os.path.join(_mypackage_root_dir, 'VERSION'))
+__version__ = _version_file.read().strip()
 
 # the following line imports all the functions from datahandling.py
 from .datahandling import *
--- datahandling/setup.py
+++ datahandling/setup.py
@@ -1,20 +1,15 @@
 from distutils.core import setup
+import os
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-# the following 8 lines set the __version__ variable
-import pkg_resources as _pkg_resources
+mypackage_root_dir = os.path.dirname(__file__)
+version_file = open(os.path.join(mypackage_root_dir, 'datahandling/VERSION'))
+version = version_file.read().strip()
 
-_resource_package = __name__  # Gets the module/package name.
-_rawVersionFile = _pkg_resources.resource_string(
-                _resource_package, "VERSION")
-_decodedVersionFile = _rawVersionFile.decode(encoding='UTF-8')
-# decocdes the bytes object into a string object
-Version = _decodedVersionFile.splitlines()[0]
-    
 setup(name='datahandling',
-      version=Version,
+      version=version,
       description='Python package with functions for data analysis',
       author='Ashley Setter',
       author_email='A.Setter@soton.ac.uk',
