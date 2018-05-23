fa98b4047cb461a1db8ceca4bcf7b8a55e620349
Markus Rademacher <markusrademacher@me.com>
2018-05-23 21:50:52 +0200

    deleted some mock modules for readthedocs

--- optoanalysis/docs/source/conf.py
+++ optoanalysis/docs/source/conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas', 'numba', 'pycuda', 'skcuda', 'numpy.ma', 'matplotlib.pyplot']
+MOCK_MODULES = ['numba', 'pycuda', 'skcuda']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
