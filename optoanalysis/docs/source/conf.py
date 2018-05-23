15c8f65d870a981bee57e2cbdfe288f7f77eca05
Markus Rademacher <markusrademacher@me.com>
2018-05-23 22:20:37 +0200

    changed mock modules for readthedocs

--- optoanalysis/docs/source/conf.py
+++ optoanalysis/docs/source/conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numba', 'pycuda', 'pycuda.autoinit', 'pycuda.gpuarray', 'skcuda']
+MOCK_MODULES = ['numba', 'pycuda', 'pycuda.autoinit', 'pycuda.gpuarray', 'skcuda.fft']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
