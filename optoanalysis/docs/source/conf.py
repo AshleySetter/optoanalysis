fe51ddb3872286f3a7feec37a887e8498b822927
Markus Rademacher <markusrademacher@me.com>
2018-05-23 21:44:10 +0200

    added matplotlib as mock module for readthedocs

--- optoanalysis/docs/source/conf.py
+++ optoanalysis/docs/source/conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas', 'numba', 'pycuda', 'skcuda', 'numpy.ma']
+MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas', 'numba', 'pycuda', 'skcuda', 'numpy.ma', 'matplotlib']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
